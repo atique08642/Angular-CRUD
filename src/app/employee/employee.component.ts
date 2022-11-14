2db315abd73d4a902234da9cbd845a078c162887
Atique Ansari <atique.ansari08642@gmail.com>
2022-11-14 15:17:26 +0530

    Added Validations

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
+  employeeObj  : EmployeeObj;
+  sortBy: String;
+  searchText: String;
+  employeeArr: EmployeeObj[] = [];
+  
+  constructor()
+  {
+    this.employeeObj = new EmployeeObj();
+    this.sortBy="";
+    this.searchText="";
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
 
+  onSave(){
+    this.employeeArr.push(this.employeeObj);
+  }
+
+}
+
+export class EmployeeObj{
+  FirstName: String;
+  LastName: String;
+  Technology: String;
+  Designation: String;
+  skill: String;
+  Core: String;
+  IsCertification: String;
+  Certification:String;
+  Company: String;
+  fewDetails: String;
+  
+  constructor(){
+  this.FirstName = "";
+  this.LastName ="";
+  this.Technology = "";
+  this.Designation = "";
+  this.skill= "";
+  this.Certification = "";
+  this.Core= "";
+  this.IsCertification= "";
+  this.Company= "";
+  this.fewDetails= "";
+  }
 }
