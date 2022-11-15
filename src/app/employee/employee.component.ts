b0540afc0c8c4c7afba89a74b4d9d144cd8eec7a
Atique Ansari <atique.ansari08642@gmail.com>
2022-11-15 16:27:45 +0530

    Implmented onSave onEdit and onDelete methods

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -20,15 +20,59 @@ export class EmployeeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getAllEmployee();
   }
 
   onSave(){
     this.employeeArr.push(this.employeeObj);
+    const isData = localStorage.getItem("EmpData");
+    if(isData == null){
+      const newArr = [];
+      this.employeeObj.EmployeeId =0;
+      newArr.push(this.employeeObj);
+      localStorage.setItem("EmpData",JSON.stringify(newArr));
+    }else{
+      const oldData = JSON.parse(isData);
+      const newId = oldData.length + 1;
+      this.employeeObj.EmployeeId = newId;
+      oldData.push(this.employeeObj);
+      localStorage.setItem("EmpData",JSON.stringify(oldData))
+    }
+    this.employeeObj = new EmployeeObj();
+    this.getAllEmployee();
+  }
+
+  getAllEmployee(){
+    const isData = localStorage.getItem("EmpData");
+    if(isData != null){
+       const localData = JSON.parse(isData);
+       this.employeeArr = localData;
+    }
+  }
+
+  onEdit(item: EmployeeObj){
+    this.employeeObj = item;
+  }
+
+  onDelete(item: EmployeeObj){
+    const isData = localStorage.getItem("EmpData");
+    if(isData != null){
+       const localData = JSON.parse(isData);
+       for (let index = 0; index < localData.length; index++) {
+        if(localData[index].EmployeeId == item.EmployeeId){
+          localData.splice(0,1);
+        }
+        
+       }
+       localStorage.setItem("EmpData",JSON.stringify(localData))
+       this.getAllEmployee();
+    }
   }
 
 }
 
 export class EmployeeObj{
+  EmployeeId: number;
   FirstName: String;
   LastName: String;
   Technology: String;
@@ -41,6 +85,7 @@ export class EmployeeObj{
   fewDetails: String;
   
   constructor(){
+  this.EmployeeId= 0;
   this.FirstName = "";
   this.LastName ="";
   this.Technology = "";
